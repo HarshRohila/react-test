35785afdc810ac8799215ebe3bd3602c9f02bfbc
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-07-31 14:46:17 +0530

    added cors

--- express-test/src/Server.ts
+++ express-test/src/Server.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import helmet from 'helmet';
 import StatusCodes from 'http-status-codes';
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -14,24 +15,22 @@ import { cookieProps } from '@shared/constants';
 const app = express();
 const { BAD_REQUEST } = StatusCodes;
 
-
-
 /************************************************************************************
  *                              Set basic express settings
  ***********************************************************************************/
-
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(cookieProps.secret));
 
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+	app.use(morgan('dev'));
 }
 
 // Security
 if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
+	app.use(helmet());
 }
 
 // Add APIs
@@ -40,13 +39,11 @@ app.use('/api', BaseRouter);
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    logger.err(err, true);
-    return res.status(BAD_REQUEST).json({
-        error: err.message,
-    });
+	logger.err(err, true);
+	return res.status(BAD_REQUEST).json({
+		error: err.message,
+	});
 });
-
-
 
 /************************************************************************************
  *                              Serve front-end content
@@ -58,19 +55,17 @@ const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
 
 app.get('/', (req: Request, res: Response) => {
-    res.sendFile('login.html', {root: viewsDir});
+	res.sendFile('login.html', { root: viewsDir });
 });
 
 app.get('/users', (req: Request, res: Response) => {
-    const jwt = req.signedCookies[cookieProps.key];
-    if (!jwt) {
-        res.redirect('/');
-    } else {
-        res.sendFile('users.html', {root: viewsDir});
-    }
+	const jwt = req.signedCookies[cookieProps.key];
+	if (!jwt) {
+		res.redirect('/');
+	} else {
+		res.sendFile('users.html', { root: viewsDir });
+	}
 });
-
-
 
 /************************************************************************************
  *                              Export Server
